dddea29cd0ea25553f416bf3858ac428dcb58251
leimo <leimo@huyongdeiMac.local>
2017-05-11 13:46:36 +0800

    Initial Commit

---
+++ CollectionViewDemo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  CollectionViewDemo
+//
+//  Created by leimo on 2017/5/11.
+//  Copyright © 2017年 胡勇. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ CollectionViewDemo/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  CollectionViewDemo
+//
+//  Created by leimo on 2017/5/11.
+//  Copyright © 2017年 胡勇. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
